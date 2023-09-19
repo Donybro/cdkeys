dc2159c72e58cb472c449c0b383e79345e3633e8
Donybro <dony1bro@gmail.com>
2023-09-20 01:23:47 +0500

    finished Merchants view

--- src/hooks/useMerchants.ts
+++ src/hooks/useMerchants.ts
@@ -1,12 +1,17 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import apiRequest from "../shared/utils/api/apiRequest";
+import { useState } from "react";
 
 export default function useMerchants() {
-  const { isLoading, data, error } = useQuery("merchants", async () => {
+  const [keyForMerchant, updateMerchantsList] = useState("merchants");
+
+  const { isLoading, data, error } = useQuery([keyForMerchant], async () => {
     return await apiRequest.get("/merchants");
   });
 
-  const { data: merchantPairs } = useQuery("comparisons", async () => {
+  const [keyForComparisons, updateComparisonsList] = useState("comparisons");
+
+  const { data: merchantPairs } = useQuery([keyForComparisons], async () => {
     return await apiRequest.get("/comparisons");
   });
 
@@ -30,11 +35,43 @@ export default function useMerchants() {
     return existSelectedMerchantPairIds;
   }
 
+  const {
+    mutate,
+    isLoading: merchantsAreAdding,
+    isSuccess: merchantAreAdded,
+    isError: merchantAreNotAdded,
+  } = useMutation({
+    mutationFn: (data: any) => {
+      return apiRequest.post("/comparisons", data);
+    },
+  });
+
+  const {
+    mutate: deletePairHandler,
+    isLoading: pairIsDeleting,
+    isSuccess: pairIsDeleted,
+    isError: pairIsNotDeleted,
+  } = useMutation({
+    mutationFn: (comparisonId: any) => {
+      return apiRequest.delete("/comparisons/" + comparisonId);
+    },
+  });
+
   return {
     merchantsAreLoading: isLoading,
     merchantsList: data?.data || [],
     merchantsListError: error,
     merchantPairs: merchantPairs?.data || [],
     getExistPairSuggestionsForSelectedMerchant,
+    addMerchantPair: mutate,
+    merchantsAreAdding,
+    merchantAreAdded,
+    merchantAreNotAdded,
+    deletePairHandler,
+    pairIsDeleting,
+    pairIsDeleted,
+    pairIsNotDeleted,
+    updateMerchantsList,
+    updateComparisonsList,
   };
 }
--- src/modules/Merchants/index.tsx
+++ src/modules/Merchants/index.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import useMerchants from "../../hooks/useMerchants";
 import MerchantItem from "../../components/UI/MerchantItem";
-import { Button } from "antd";
+import { Button, message, Space } from "antd";
 import styles from "./style.module.scss";
 import ComparisonCard from "../../components/UI/ComparisonCard";
 
@@ -10,13 +10,58 @@ const Index: FC = () => {
     getExistPairSuggestionsForSelectedMerchant,
     merchantsList,
     merchantPairs,
+    addMerchantPair,
+    merchantAreAdded,
+    merchantAreNotAdded,
+    merchantsAreAdding,
+    deletePairHandler,
+    pairIsDeleting,
+    pairIsDeleted,
+    pairIsNotDeleted,
+    updateComparisonsList,
+    updateMerchantsList,
   } = useMerchants();
 
   const [firstSelectedMerchant, setFirstSelectedMerchant] = useState<any>();
   const [secondSelectedMerchant, setSecondSelectedMerchant] = useState<any>();
 
+  const [selectedPair, setSelectedPair] = useState<any>();
+
   const [existPairsForSelectedMerchant, setExistPairsForSelectedMerchant] =
     useState([]);
+
+  useEffect(() => {
+    if (merchantAreAdded) {
+      clearSelectedMerchants();
+      message.success("Merchant added!");
+      updateLists();
+    }
+  }, [merchantAreAdded]);
+
+  useEffect(() => {
+    if (pairIsDeleted) {
+      setSelectedPair("");
+      updateLists();
+      message.success("Pair deleted!");
+    }
+  }, [pairIsDeleted]);
+
+  useEffect(() => {
+    if (merchantAreNotAdded) {
+      message.error("Something get wrong!");
+    }
+  }, [merchantAreNotAdded]);
+
+  useEffect(() => {
+    if (pairIsNotDeleted) {
+      message.error("Something get wrong!");
+    }
+  }, [pairIsNotDeleted]);
+
+  function updateLists() {
+    updateComparisonsList((prevState) => prevState + "1");
+    updateMerchantsList((prevState) => prevState + "1");
+  }
 
   const onSelectMerchant = (merchant_id: any) => {
     if (existPairsForSelectedMerchant.includes(merchant_id)) {
@@ -39,7 +84,15 @@ const Index: FC = () => {
   };
 
   const submitMerchants = async () => {
-    console.log(firstSelectedMerchant, secondSelectedMerchant);
+    const payload = {
+      merchant_one: firstSelectedMerchant,
+      merchant_two: secondSelectedMerchant,
+    };
+    addMerchantPair(payload);
+  };
+
+  const deleteSelectedPair = () => {
+    deletePairHandler(selectedPair);
   };
 
   return (
@@ -68,21 +121,31 @@ const Index: FC = () => {
         ))}
       </div>
       <br />
-      <Button onClick={() => clearSelectedMerchants()}>Cancel selected</Button>
-      <Button
-        disabled={!(firstSelectedMerchant && secondSelectedMerchant)}
-        type={"primary"}
-        onClick={submitMerchants}
-      >
-        Submit
-      </Button>
+      <Space size={10}>
+        <Button onClick={() => clearSelectedMerchants()}>
+          Cancel selected
+        </Button>
+        <Button
+          disabled={
+            !(firstSelectedMerchant && secondSelectedMerchant) ||
+            merchantsAreAdding
+          }
+          loading={merchantsAreAdding}
+          type={"primary"}
+          onClick={submitMerchants}
+        >
+          Submit
+        </Button>
+      </Space>
       <br />
       <h1>List of merchant pairs</h1>
       <div className={styles.merchantsList}>
         {merchantPairs.map((pair) => (
           <div
-            className={`${styles.merchant}`}
-            onClick={() => onSelectMerchant(merchant.merchant_id)}
+            className={`${styles.merchant} ${
+              selectedPair === pair.id ? styles.selected : null
+            }`}
+            onClick={() => setSelectedPair(pair.id)}
           >
             <ComparisonCard
               merchantOne={pair?.merchant_one}
@@ -92,6 +155,15 @@ const Index: FC = () => {
           </div>
         ))}
       </div>
+      <br />
+      <Button
+        disabled={pairIsDeleting}
+        loading={pairIsDeleting}
+        type={"dashed"}
+        onClick={deleteSelectedPair}
+      >
+        Delete selected pair
+      </Button>
     </div>
   );
 };
