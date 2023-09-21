758a0fba4a3c532ab20ee1437916c3e9c35e2c73
Donybro <dony1bro@gmail.com>
2023-09-21 19:36:29 +0500

    added pagination

--- src/components/Games/OfferCard/index.tsx
+++ src/components/Games/OfferCard/index.tsx
@@ -17,7 +17,7 @@ const Index: FC = ({ merchantInfo, offerInfo, regionInfo, editionInfo }) => {
           <div className={styles.merchantName}>{merchantInfo.name}</div>
         </div>
         <div className={styles.platformWrapper}>
-          <span className={styles.region}>{regionInfo.name}</span>
+          <span className={styles?.region}>{regionInfo.name}</span>
           <span
             className={`${styles.platformLogo} sprite sprite-30-${offerInfo.platform}`}
           ></span>
